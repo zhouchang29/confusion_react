09d38e6e78f886b906fe179aaf2c57a8411b1b39
格莱 <zhouchang29@gmail.com>
2019-05-25 18:26:20 +0800

    Assignment 1 new

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -18,7 +18,7 @@ class Menu extends Component {
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 col-md-5 mt-1">
+                <div key={dish.id} className="col-12 col-md-5 m-1">
                     <Card onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
