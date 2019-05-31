c28bc831dcd50067f88163c24927b32c81404b19
格莱 <zhouchang29@gmail.com>
2019-05-31 11:31:26 +0800

    Fetch from Server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,5 @@
 import * as ActionTypes from './ActionTypes';
-import { DISHES } from '../shared/dishes'; 
+import { baseUrl } from '../shared/baseUrl';
 
 export const addComment = (dishId, rating, author, comment) => ({
   type: ActionTypes.ADD_COMMENT,
@@ -13,10 +13,15 @@ export const addComment = (dishId, rating, author, comment) => ({
 
 export const fetchDishes = () => (dispatch) => {
   dispatch(dishesLoading(true));
+  return fetch(baseUrl + 'dishes')
+  .then(response => response.json())
+  .then(dishes => dispatch(addDishes(dishes)));
+}
 
-  setTimeout(() => {
-    dispatch(addDishes(DISHES));
-  }, 2000);
+export const fetchComments = () => (dispatch) => {
+  return fetch(baseUrl + 'comments')
+  .then(response => response.json())
+  .then(comments => dispatch(addComments(comments)));
 }
 
 export const dishesLoading = () => ({
@@ -31,4 +36,35 @@ export const dishesFailed = (errmess) => ({
 export const addDishes = (dishes) => ({
   type: ActionTypes.ADD_DISHES,
   payload: dishes
+});
+
+export const commentsFailed = (errmess) => ({
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: errmess
+});
+
+export const addComments = (comments) => ({
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments
+});
+
+export const fetchPromos = () => (dispatch) => {
+  dispatch(promosLoading(true));
+  return fetch(baseUrl + 'promotions')
+  .then(response => response.json())
+  .then(promos => dispatch(addPromos(promos)));
+}
+
+export const promosLoading = () => ({
+  type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+  type: ActionTypes.PROMOS_FAILED,
+  payload: errmess
+});
+
+export const addPromos = (promos) => ({
+  type: ActionTypes.ADD_PROMOS,
+  payload: promos
 });
