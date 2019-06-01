25571c2c9b16179b47a3a66baaf7aa5d6c7a4376
格莱 <zhouchang29@gmail.com>
2019-06-01 16:13:12 +0800

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -24,7 +24,7 @@ class CommentForm extends React.Component {
 
 	handleSubmit(values) {
 		this.toggleModal();
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 	}
 
 	toggleModal(){
@@ -110,7 +110,7 @@ function RenderDish({dish}) {
 	}
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
 	if (comments && comments.length) {
 		return (
 			<div className="col-12 col-md-5 m-1">
@@ -126,7 +126,7 @@ function RenderComments({comments, addComment, dishId}) {
 						)
 					}) }
 				</ul>
-				<CommentForm dishId={dishId} addComment={addComment}/>
+				<CommentForm dishId={dishId} postComment={postComment}/>
 			</div>
 		);
 	} else {
@@ -134,7 +134,7 @@ function RenderComments({comments, addComment, dishId}) {
 			<div>	
 				<div className="col-12 col-md-5 m-1">
 					<h4>Comments</h4>
-					<CommentForm dishId={dishId} addComment={addComment}/>
+					<CommentForm dishId={dishId} postComment={postComment}/>
 				</div>
 			</div>
 		);
@@ -177,7 +177,7 @@ const DishDetail = (props) => {
 			<div className="row">
 				<RenderDish dish={props.dish}/>
 				<RenderComments comments={props.comments} 
-				  addComment={props.addComment} dishId={props.dish.id}/>
+				  postComment={props.postComment} dishId={props.dish.id}/>
 			</div>
 		</div>
 	);
